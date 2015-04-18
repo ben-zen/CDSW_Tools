333564da30f9daa6cc3bd24557dbf300cf1b80ec
Ben Lewis <benjf5+github@gmail.com>
2015-04-18 16:52:44 -0700

    Added some first-pass metrics to the Workshop object and did a bit of cleanup.

--- cdsw_attendee.py
+++ cdsw_attendee.py
@@ -13,24 +13,28 @@ class Attendee:
     def __init__(self, email):
         self.EmailAddress = email
         self.Lectures = [False, False, False, False]
-        self.Sessions = ["", "", ""]
+        self.Sessions = ["", "", "", ""]
 
     def Lecture_sign_in(self, day):
         if not self.Lectures[day]:
             self.Lectures[day] = True
         else:
             raise DoubleWriteException(self.EmailAddress, day)
-            print(self.EmailAddress + " attempted to sign in twice!")
 
     def Session_sign_in(self, day, session):
         if self.Sessions[day] is "":
             self.Sessions[day] = session
         else:
             raise DoubleWriteException(self.EmailAddress, day, session)
-            print(self.EmailAddress + " attempted to sign in twice!")
 
 class MissingAttendeeException(Exception):
     """Used when a call that needs an attendee fails to locate one."""
+
+    def __init__(self, email):
+        self.EmailAddress = email
+
+class PreexistingAttendeeException(Exception):
+    """Represents attempting to create an attendee that already exists."""
 
     def __init__(self, email):
         self.EmailAddress = email
@@ -45,3 +49,18 @@ class Workshop:
             return self.Attendees[email]
         else:
             raise MissingAttendeeException(email)
+
+    def Add_attendee(self, email):
+        if email not in self.Attendees:
+            self.Attendees[email] = Attendee(email)
+        else:
+            raise PreexistingAttendeeException(email)
+
+    def Show_attendance_by_session(self, day):
+        sessions = {}
+        for x in self.Attendees.values():
+            if x.Sessions[day] in sessions:
+                sessions[x.Sessions[day]] = sessions[x.Sessions[day]] + 1
+            else:
+                sessions[x.Sessions[day]] = 1
+        return sessions
