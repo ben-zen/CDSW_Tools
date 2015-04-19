4ff5f749d30e5fca13c94b5ecce9b35973f6678b
Ben Lewis <benjf5+github@gmail.com>
2015-04-18 22:32:08 -0700

    Cleaned up documentation issues raised by adding more docs, also fixed a small bug that would leave empty string in the list of workshops attended.

--- cdsw_attendee.py
+++ cdsw_attendee.py
@@ -1,3 +1,10 @@
+"""Provides useful classes for logging information about a CDSW session.
+
+The session itself is managed through the Workshop class; an instance of that
+object can be used to create new attendees and insert them into its
+dictionary. The Workshop class can also generate metrics about attendees to
+gauge continued involvement and interest.
+"""
 # Copyright (C) 2015 Ben Lewis
 # Licensed under the MIT license.
 
@@ -38,35 +45,30 @@ class Attendee:
     """
 
     def __init__(self, email):
-
         """Setup method for the Attendee object.
 
         Keyword arguments:
         email -- The identifier for the attendee, for lookup and comparison.
         """
-
         self.EmailAddress = email
         self.Lectures = [False, False, False, False]
         self.Sessions = ["", "", "", ""]
 
     def Lecture_sign_in(self, day):
-
         """Sign-in function for a lecture on a given day of the workshop.
 
         Each attendee can sign in at most once for that mornings' lecture;
-        any furtherr attempt to sign in will throw a DoubleWriteException.
+        any further attempt to sign in will throw a DoubleWriteException.
 
         Keyword arguments:
         day -- The day of the workshop (ordinal, not date.)
         """
-
         if not self.Lectures[day]:
             self.Lectures[day] = True
         else:
             raise DoubleWriteException(self.EmailAddress, day)
 
     def Session_sign_in(self, day, session):
-
         """Sign-in method for afternoon Workshop sessions.
 
         Each day of the workshop, an attendee can sign in to at most one
@@ -77,7 +79,6 @@ class Attendee:
         day     -- The day of the workshop (ordinal, not date.)
         session -- The name of the session attended.
         """
-
         if self.Sessions[day] is "":
             self.Sessions[day] = session
         else:
@@ -99,25 +100,59 @@ class PreexistingAttendeeException(Exception):
 
 class Workshop:
 
+    """Stores all attendees and generates metrics for a CDSW session.
+
+    Maintains a dictionary of Attendee objects with email addresses as keys, and
+    provides methods to provide metrics on the Attendees.
+
+    Members:
+    Attendees -- Dictionary of attendees, using email addresses as primary keys.
+    """
+
     def __init__(self):
         self.Attendees = {}
 
     def Find_attendee(self, email):
+        """Searches for an attendee in the dictionary.
+
+        On failure, this method raises an MissingAttendeeException.
+
+        Keyword arguments:
+        email -- Email address for lookup in the dictionary.
+        """
         if email in self.Attendees:
             return self.Attendees[email]
         else:
             raise MissingAttendeeException(email)
 
     def Add_attendee(self, email):
+        """Add an attendee to the workshop, using their email address as key.
+
+        If the email address is already present in the dictionary, this method
+        raises a PreexistingAttendeeException.
+
+        Keyword arguments:
+        email -- Email address for lookup and Attendee creation.
+        """
         if email not in self.Attendees:
             self.Attendees[email] = Attendee(email)
         else:
             raise PreexistingAttendeeException(email)
 
     def Show_attendance_by_session(self, day):
+        """Compute how many attendees signed in to each session on a day.
+
+        For every attendee that signs in to one of the offered sessions, this
+        method increments the counter for that session (or initializes it to one
+        for a session that hasn't been encountered yet.
+
+        Keyword arguments:
+        day -- the day of the workshop for generating metrics (ordinal, not
+               date.)
+        """
         sessions = {}
         for x in self.Attendees.values():
-            if x.Sessions[day] in sessions:
+            if (x.Sessions[day] in sessions) and (x.Sessions[day] is not ""):
                 sessions[x.Sessions[day]] = sessions[x.Sessions[day]] + 1
             else:
                 sessions[x.Sessions[day]] = 1
